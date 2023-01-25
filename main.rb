43a8c4ac6885901bdc68e39eb2813b2a8f2d9701
Maira Berlato <mairaberlatof@gmail.com>
2023-01-25 09:11:54 -0300

    define functions

--- main.rb
+++ main.rb
@@ -6,15 +6,22 @@ GRID_COLOR = Color.new('#222222')
 GRID_SIZE = 40
 LINE_WIDTH = 2
 
-set width: BLOCK_WIDTH * GRID_SIZE,
-    height: BLOCK_HEIGHT * GRID_SIZE
+def draw_grid
+  (0..Window.width).step(GRID_SIZE).each do |x|
+    Line.new(x1: x, x2: x, y1: 0, y2: Window.height, width: LINE_WIDTH, color: GRID_COLOR)
+  end
 
-(0..Window.width).step(GRID_SIZE).each do |x|
-  Line.new(x1: x, x2: x, y1: 0, y2: Window.height, width: LINE_WIDTH, color: GRID_COLOR)
+  (0..Window.height).step(GRID_SIZE).each do |y|
+    Line.new(x1: 0, x2: Window.width, y1: y, y2: y, width: LINE_WIDTH, color: GRID_COLOR)
+  end
 end
 
-(0..Window.height).step(GRID_SIZE).each do |y|
-  Line.new(x1: 0, x2: Window.width, y1: y, y2: y, width: LINE_WIDTH, color: GRID_COLOR)
+def main
+  set width: BLOCK_WIDTH * GRID_SIZE,
+      height: BLOCK_HEIGHT * GRID_SIZE
+
+  draw_grid
+  show
 end
 
-show
+main
